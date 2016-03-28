4842dfd73eada1a2d90dc2c1f17c6c31219417ce
Evan Klemen <evan@Janes-MacBook-Pro-4.local>
2016-03-27 21:57:27 -0400

    images optimized and added to gallery

--- src/app/gallery/gallery.controller.js
+++ src/app/gallery/gallery.controller.js
@@ -8,32 +8,79 @@
   /** @ngInject */
   function GalleryController($mdDialog) {
     var self = this;
-      self.images =[
+      self.engagement =[
         {
-          src: 'assets/images/flower-unlit.JPG',
-          title: '',
-          caption: ''
+          src: 'assets/images/engagement/engagement1.JPG',
+          title: ''
         },
         {
-          src: 'assets/images/flower-lit.JPG',
-          title: '',
-          caption: ''
+          src: 'assets/images/engagement/engagement2.JPG',
+          title: ''
         },
         {
-          src: 'assets/images/flower2-sm.JPG',
-          title: '',
-          caption: ''
+          src: 'assets/images/engagement/engagement3.JPG',
+          title: ''
         },
         {
-          src: 'assets/images/flower3-sm.JPG',
-          title: '',
-          caption: ''
-        },
-        {
-          src: 'assets/images/flower3-sm.JPG',
-          title: '',
-          caption: ''
+          src: 'assets/images/engagement/engagement4.JPG',
+          title: ''
         }
-      ]
+      ];
+
+      self.graduation =[
+        {
+          src: 'assets/images/graduation/graduation1.jpg',
+          title: ''
+        },
+        {
+          src: 'assets/images/graduation/graduation2.jpg',
+          title: ''
+        }
+      ];
+
+      self.panels =[
+        {
+          src: 'assets/images/panel/panel1.jpg',
+          title: ''
+        },
+        {
+          src: 'assets/images/panel/panel2.jpg',
+          title: ''
+        },
+        {
+          src: 'assets/images/panel/panel3.jpg',
+          title: ''
+        }
+      ];
+
+      self.letters =[
+        {
+          src: 'assets/images/letter/letter1.jpg',
+          title: ''
+        },
+        {
+          src: 'assets/images/letter/letter2.jpg',
+          title: ''
+        }
+      ];
+
+      self.store =[
+        {
+          src: 'assets/images/store/store1.JPG',
+          title: ''
+        },
+        {
+          src: 'assets/images/store/store2.JPG',
+          title: ''
+        },
+        {
+          src: 'assets/images/store/store3.JPG',
+          title: ''
+        },
+        {
+          src: 'assets/images/store/store4.JPG',
+          title: ''
+        }
+      ];
   }
 })();
